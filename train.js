457062f3fdddd60f8e4534788418f750f47f4e40
martin <martin@comets.kr>
2023-09-16 21:19:47 +0900

    fix: modified train.js

--- train.js
+++ train.js
@@ -4,5 +4,9 @@ console.log("Bu yerda task bajarildi!!!!");
 
 git add .
 git commit -m "feat:"
+git status
 
+// push qilmagan bolsaiz
 git commit --amend
+git commit --amend -m "fix:"
+git push origin master --force
